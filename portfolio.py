5498330202b73daa22e4e400e686c65384b196d6
pozou <paul.guilloteau@epita.fr>
2020-11-24 15:17:44 +0100

    [UPDATE] chek_portfolio_conditions check amount assets <= 50%

--- portfolio.py
+++ portfolio.py
@@ -17,10 +17,17 @@ def check_portfolio_conditions(portfolio_id):
     portfolio = restManager.get_portfolio(portfolio_id)
     values = portfolio['values']
     portfolio_size = len(values)
+    portfolio_amount = 0
     if (portfolio_size < nb_asset_min) :
         print("portfolio size: " + str(portfolio_size) + " is less than the minimal required size: " + str(nb_asset_min))
         return False
     if (portfolio_size > nb_asset_max):
         print("portfolio size: " + str(portfolio_size) + " is more than the maximal required size: " + str(nb_asset_max))
+        return False
+    nb_asset = 0
+    for i in values:
+        if values[i]['TYPE']['value'] == 'STOCK': # à tester
+            nb_asset += 1
+    if (portfolio_size // nb_asset) < 2.0: # voir si ça fait bien la virgule
         return False
     return True
