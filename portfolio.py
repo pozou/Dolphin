7c870832c2a807fa3505a19e08891715ef984d10
pozou <paul.guilloteau@epita.fr>
2020-11-24 15:12:00 +0100

    [ADD] check_porfolio_conditions verify porfolio size

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,8 @@
 import restManager
 
 nb_actif = 20
+nb_asset_min = 15
+nb_asset_max = 40
 
 def generate_portfolio():
     portfolio = []
@@ -10,3 +12,15 @@ def generate_portfolio():
         portfolio.append(list_asset[i])
         i += 1
     return portfolio # Bad Object
+
+def check_portfolio_conditions(portfolio_id):
+    portfolio = restManager.get_portfolio(portfolio_id)
+    values = portfolio['values']
+    portfolio_size = len(values)
+    if (portfolio_size < nb_asset_min) :
+        print("portfolio size: " + str(portfolio_size) + " is less than the minimal required size: " + str(nb_asset_min))
+        return False
+    if (portfolio_size > nb_asset_max):
+        print("portfolio size: " + str(portfolio_size) + " is more than the maximal required size: " + str(nb_asset_max))
+        return False
+    return True
