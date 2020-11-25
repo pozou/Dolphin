1cca82bf03cee258a0da2c9c2605644f29cf7d0b
pozou <paul.guilloteau@epita.fr>
2020-11-25 19:03:13 +0100

    [main] change date end sharpo portfolio test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import reference
 
 if __name__ == "__main__":
     print("Main function")
-    pprint(restManager.invoke_ratio([portfolio.sharpe_id], [1832], 0, "2016-06-20", "2019-06-20"))
+    pprint(restManager.invoke_ratio([portfolio.sharpe_id], [1832], 0, portfolio.period_start_date, portfolio.period_end_date))
     res = restManager.get_asset(1855, "2019-06-01")
     portfolio = portfolio.generate_portfolio()
     #restManager.update_portfolio(restManager.portfolio_id, portfolio)
