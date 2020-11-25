b44a9b9ead5b95f25fa4b607d15fdc9d7636c073
JulesLa <jules.lapisardi@epita.fr>
2020-11-25 22:17:06 +0100

    Calculate money spend

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import reference
 if __name__ == "__main__":
     print("Main function")
     res = restManager.get_asset(1855, "2019-06-01")
-    #portfolio = portfolio.generate_portfolio()
-    pprint(portfolio.sharpe_id)
-    pprint(restManager.get_portfolio(restManager.portfolio_id))
+    #port = portfolio.generate_portfolio()
+    #pprint(portfolio.sharpe_id)
     pprint(restManager.invoke_ratio([portfolio.sharpe_id], [restManager.portfolio_id], 0, portfolio.period_start_date,
                                     portfolio.period_end_date))
+    pprint(portfolio.check_portfolio_conditions(restManager.portfolio_id))
+    pprint(restManager.get_portfolio(restManager.portfolio_id))
     #pprint(restManager.get_list_stock())
-    #restManager.update_portfolio(restManager.portfolio_id, portfolio)
+    #restManager.update_portfolio(restManager.portfolio_id, port)
     #pprint(restManager.get_last_close_value(1900, "2019-06-20"))
     #pprint(restManager.get_last_close_value(1900, "2016-06-20"))
     #pprint(restManager.get_quote_list(1900, "2016-06-20", "2019-06-20"))
--- portfolio.py
+++ portfolio.py
@@ -25,6 +25,7 @@ def generate_portfolio():
     list_asset = restManager.get_list_stock()
     i = 0
     money_total = 10000
+    money_res = 0
     while len(portfolio) < nb_actif:
         try:
             if list_asset[i]['CURRENCY']['value'] != 'EUR':
@@ -41,13 +42,14 @@ def generate_portfolio():
         #
         # ratio de sharpe d'un actif
         sharpe_actif = float(restManager.invoke_ratio([sharpe_id], [asset_id], 0, period_start_date, period_end_date)[str(asset_id)][str(sharpe_id)]['value'].replace(',', '.'))
-        pprint(sharpe_actif)
         if  sharpe_actif > 0.8:
             tmp = {"quantity": quantity, "asset": int(list_asset[i]["ASSET_DATABASE_ID"]["value"])}
             portfolio.append(tmp)
+            money_res += quantity * value
             print("len portofolio : ", len(portfolio))
         i += 1
     print("PORTFOLIO GENERATED")
+    print(money_res)
     return portfolio
 
 
@@ -73,7 +75,7 @@ Conditions :
 
 def check_portfolio_conditions(portfolio_id):
     portfolio = restManager.get_portfolio(portfolio_id)
-    values = portfolio["values"]
+    values = portfolio["values"][period_start_date]
     portfolio_size = len(values)
     portfolio_amount = 0
     if (portfolio_size < nb_asset_min):
